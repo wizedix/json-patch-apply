479ff58626a97ba5ab1e4b2e6523d26db610ab30
hakalab <wizedix@gmail.com>
2022-10-05 12:52:41 -0700

    Fixed tests, worked in the rest of the features.  Removed the non-standard parts of the library.

--- tests/common.spec.ts
+++ tests/common.spec.ts
@@ -1,45 +1,51 @@
 import {expect} from "chai";
-import {firstElement, getValue, getValueType, ValueType} from "../src/common";
+import {PatchDiff} from "../src/common";
 
 describe("common", () => {
-    describe("firstElement", () => {
-        it("errors out with firstElement of empty", () => {
-            try {
-                firstElement([]);
-                throw new Error("Missing error");
-            } catch (e) {
-                expect(e.message).eql("cannot get first of empty list");
-            }
-        });
-    });
+    describe("PatchDiff", () => {
+        let t = new PatchDiff();
 
-    describe("getValue", () => {
-        it("returns value when present", () => {
-            let a = {b: 123};
-            expect(getValue(a, "b")).eql(123);
-        });
+        describe("can determine if object has key", () => {
+            let o: any = {"a":1,"b":2, 3:3}
+            it("when key is present", () => {
+                expect(t.hasKey(o, "a")).eql(true)
+                expect(t.hasKey(o, "b")).eql(true)
+                expect(t.hasKey(o, 3)).eql(true)
+            })
+            it("when key is present", () => {
+                expect(t.hasKey(o, "c")).eql(false)
+                expect(t.hasKey(o, 0)).eql(false)
+                expect(t.hasKey(o, "c")).eql(false)
+            })
+        })
+        describe("can determine if array has key", () => {
+            let a: any = [1,2]
+            it("when key is present", () => {
+                expect(t.hasKey(a, 0)).eql(true)
+                expect(t.hasKey(a, 1)).eql(true)
+            });
+            it("when key is not present", () => {
+                expect(t.hasKey(a, -1)).eql(false)
+                expect(t.hasKey(a, 2)).eql(false)
+            });
+        })
 
-        it("returns undefined when value not present", () => {
-            let a = {};
-            expect(getValue(a, "b")).be.undefined;
-        });
-    });
+        it("can get parent path", () => {
+            expect("").eql(t.getParentPath(""))
+            expect("").eql(t.getParentPath("/a"))
+            expect("/a").eql(t.getParentPath("/a/b"))
+            expect("/a/0").eql(t.getParentPath("/a/0/b"))
+            expect("/a/~0x").eql(t.getParentPath("/a/~0x/b"))
+        })
 
-    describe("getValueType", () => {
-        it("returns array for array", () => {
-            expect(ValueType.array).eql(getValueType([]));
-        });
-
-        it("returns object for object", () => {
-            expect(ValueType.object).eql(getValueType({}));
-        });
-
-        it("returns primitive for other types", () => {
-            expect(ValueType.primitive).eql(getValueType(123));
-            expect(ValueType.primitive).eql(getValueType("abc"));
-            expect(ValueType.primitive).eql(getValueType(true));
-            expect(ValueType.primitive).eql(getValueType(false));
-            expect(ValueType.undefined).eql(getValueType(undefined));
-        });
-    });
+        it("can get key", () => {
+            expect("").eql(t.getKey(""))
+            expect("a").eql(t.getKey("/a"))
+            expect("b").eql(t.getKey("/a/b"))
+            expect("0").eql(t.getKey("/0"))
+            expect("1").eql(t.getKey("/0/1"))
+            expect("X~Y").eql(t.getKey("X~0Y"))
+            expect("X/Y").eql(t.getKey("X~1Y"))
+        })
+    })
 });
